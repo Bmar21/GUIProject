b43ef7ef7134b056ba22bd2474d2d7221d1a8e8e
Bmar21 <brandonmalor21@gmail.com>
2016-11-01 12:51:28 -0600

    added a colors and random colors button to the panel for the window

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -1,17 +1,23 @@
 package gui.view;
 
-import  javax.swing.JPanel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Color;
 import gui.controller.GUIController;
 
 public class FirstPanel extends JPanel
 {
 	private GUIController baseController;
+	private JButton colorButton;
+	private JButton randomButton;
 	
 	public FirstPanel(GUIController baseController) 
 	{
 		super();
 		this.baseController = baseController;
 		
+		this.colorButton = new JButton("colors!");
+		this.randomButton = new JButton("random colors!!");
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -19,7 +25,9 @@ public class FirstPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		
+		this.setBackground(Color.CYAN);
+		this.add(colorButton);
+		this.add(randomButton);
 	}
 	
 	private void setupLayout()
