98d4e8c4ff07031cc2cd48f58d954a7b017f7471
Bmar21 <brandonmalor21@gmail.com>
2016-11-01 12:31:12 -0600

    made the panel to put into the window

--- src/gui/view/FirstFrame.java
+++ src/gui/view/FirstFrame.java
@@ -1,5 +1,28 @@
 package gui.view;
 
-public class FirstFrame {
+import javax.swing.JFrame; 
+import java.awt.Dimension;
+import gui.controller.GUIController;
 
+public class FirstFrame extends JFrame
+{
+	private GUIController baseController;
+	private FirstPanel appPanel;
+	
+	public FirstFrame(GUIController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		this.appPanel = new FirstPanel(baseController);
+		
+		this.setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setTitle("OMG - Windows!!");
+		this.setSize(new Dimension(600, 400));
+		this.setVisible(true);
+	}
 }
